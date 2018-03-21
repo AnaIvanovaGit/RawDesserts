5f0233f4d121032feda70ecf7202586f9ebee52b
Ana <ananutrivanova@gmail.com>
2018-03-21 15:15:17 +0200

    visual studio changes

---
+++ Desserts/Desserts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desserts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dessert menu");
+        }
+    }
+}
